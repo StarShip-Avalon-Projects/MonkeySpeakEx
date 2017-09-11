8c0f8b90c791a768c330351929900683e807abe5
Jeremy Trombley <gerolkae@hotmail.com>
2017-09-10 22:22:47 -0700

    Update Build System

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Starship Avalon Projects")]
 [assembly: AssemblyProduct("Monkey Speak Extended Engine")]
-[assembly: AssemblyCopyright("Copyright ©  2012 - 2017")]
+
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,11 +19,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("bedba5f0-e0cf-4ec2-b4bc-bcddcdaaca42")]
 
-// Version information for an assembly consists of the following four values:
-//
-// Major Version Minor Version Build Number Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers by using the '*'
-// as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
