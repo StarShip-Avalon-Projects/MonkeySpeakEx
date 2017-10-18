714ae2fbcea54a778ec402648d2edcf710d95ae6
Jeremy Trombley <gerolkae@hotmail.com>
2017-10-17 23:41:50 -0700

    fix repo

--- lexical/AbstractLexer.cs
+++ lexical/AbstractLexer.cs
@@ -8,17 +8,41 @@ namespace Monkeyspeak
     {
         protected readonly SStreamReader reader;
 
-        protected AbstractLexer(SStreamReader reader)
+        protected AbstractLexer(MonkeyspeakEngine engine, SStreamReader reader)
         {
             this.reader = reader;
+            Engine = engine;
         }
+
+        public virtual MonkeyspeakEngine Engine { get; private set; }
 
         public virtual SourcePosition CurrentSourcePosition
         {
             get;
         }
 
+        /// <summary>
+        /// Advances one character in the reader.
+        /// </summary>
+        protected abstract void Next();
+
+        /// <summary>
+        /// Reads the tokens from the reader.  Used by the Parser.
+        /// </summary>
+        /// <returns></returns>
         public abstract IEnumerable<Token> Read();
+
+        /// <summary>
+        /// Reads the specified start position in stream.  Used by the Token to read the token's value.
+        /// </summary>
+        /// <param name="startPosInStream">The start position in stream.</param>
+        /// <param name="length">The length.</param>
+        /// <returns></returns>
+        public abstract char[] Read(long startPosInStream, int length);
+
+        protected abstract int LookAhead(int amount);
+
+        protected abstract int LookBack(int amount);
 
         public abstract void Reset();
 
@@ -27,7 +51,5 @@ namespace Monkeyspeak
             reader.Dispose();
             GC.SuppressFinalize(this);
         }
-
-        public abstract char[] Read(long valueStart, int valueLength);
     }
 }
